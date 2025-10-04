9f270dfcf5f72aa45bab8fce781016c3f4359677
chaosmethod <60120308+chaosmethod@users.noreply.github.com>
2025-10-04 16:18:14 -0500

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,8 @@
+{
+  "title": "RabbitDex",
+  "url": "https://rabbit-dex-blue.vercel.app/",
+  "description": "A digital book for collecting pok...creatures",
+  "iconUrl": "https://raw.githubusercontent.com/chaosmethod/RabbitDex/main/icon.png",
+  "themeColor": "#ff5e00"
+}
+
